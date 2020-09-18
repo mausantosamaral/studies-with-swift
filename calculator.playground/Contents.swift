5cc78e51ffa26a13b948049e93230f574f19f6a1
mausantosamaral <mausantosamaral@gmail.com>
2020-09-18 18:44:04 -0300

    Exercise 1.0

---
+++ calculator.playground/Contents.swift
@@ -0,0 +1,42 @@
+var resultado = 0
+
+
+//SOMA
+
+func soma (x: Int, y: Int) -> Int {
+    return x + y
+}
+
+
+resultado = soma(x: 30, y: 40)
+print(resultado)
+
+
+//DIVISÃO
+
+func division (x: Int, y: Int) -> Int {
+    return x / y
+}
+
+resultado = division(x: 15, y: 3)
+print(resultado)
+
+
+//MULTIPLICAÇÃO
+
+func multiplication (x: Int, y: Int) -> Int {
+    return x * y
+}
+
+resultado = multiplication(x: 100, y: 4)
+print(resultado)
+
+
+//SUBTRAÇÃO
+
+func subtration (x: Int, y: Int) -> Int {
+    return x - y
+}
+
+resultado = subtration(x: 150, y: 75)
+print(resultado)
