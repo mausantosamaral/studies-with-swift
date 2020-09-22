1cd03ad569a697c6ac734ae9b6a862c2005bbfe1
Mauricio Santos Amaral <mausantosamaral@gmail.com>
2020-09-22 11:58:16 -0300

    Add files via upload

---
+++ weekday.playground/Contents.swift
@@ -0,0 +1,21 @@
+//  Escreva um algoritmo que leia um número (1 a 7) e escreva o dia da semana correspondente. Caso o usuário digite um número fora do intervalo válido, deve ser escrita a mensagem “Dia da semana inválido”.
+
+let diaDaSemana = 1
+
+if diaDaSemana == 1 {
+    print("Domingo")
+} else if diaDaSemana == 2 {
+    print("Segunda-Feira")
+} else if diaDaSemana == 3 {
+    print("Terça-Feira")
+} else if diaDaSemana == 4 {
+    print("Quarta-Feira")
+} else if diaDaSemana == 5 {
+    print("Quinta-Feira")
+} else if diaDaSemana == 6 {
+    print("Sexta-Feira")
+} else if diaDaSemana == 7 {
+    print("Sábado")
+} else {
+   print("Dia da semana inválido")
+}
