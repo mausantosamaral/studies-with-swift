0315d566e83518216bd84eda8e56d9aeb07e655f
Mauricio Santos Amaral <mausantosamaral@gmail.com>
2020-09-28 13:57:31 -0300

    Add files via upload

---
+++ 1to500.playground/Contents.swift
@@ -0,0 +1,7 @@
+// Escreva um algoritmo que escreva todos os números múltiplos de 5, no intervalo fechado de 1 a 500.
+
+for index in 1...500 {
+    if index % 5 == 0 {
+        print(index)
+    }
+}
